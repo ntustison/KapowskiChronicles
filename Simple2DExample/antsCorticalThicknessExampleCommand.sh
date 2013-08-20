7522bce3e0add6dba768b8338428a81aa69e0816
Nick Tustison <ntustison@gmail.com>
2013-08-20 15:08:41 -0400

    ENH:  Added simple 2-D example.

---
+++ Simple2DExample/antsCorticalThicknessExampleCommand.sh
@@ -0,0 +1,19 @@
+#! /bin/sh
+
+DATA_DIR=./
+TEMPLATE_DIR=${DATA_DIR}/template/
+OUT_DIR=${DATA_DIR}/output/
+
+# set ANTSPATH
+export ANTSPATH=/Users/ntustison/Pkg/ANTs/bin/bin/
+
+bash ${ANTSPATH}antsCorticalThickness.sh -d 2 \
+  -a ${DATA_DIR}KKI2009-01-MPRAGE_slice150.nii.gz \
+  -e ${TEMPLATE_DIR}template_slice80.nii.gz \
+  -m ${TEMPLATE_DIR}template_cerebrum_mask_slice80.nii.gz \
+  -p ${TEMPLATE_DIR}prior%d_slice80.nii.gz \
+  -f ${TEMPLATE_DIR}template_extraction_mask_slice80.nii.gz \
+  -w 0.25 \
+  -n 3 \
+  -k 0 \
+  -o ${OUT_DIR}antsCorticalThicknessExample_
